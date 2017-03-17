477469c3c430f6d6b6bc6e1d707fd4a416ba7e2d
Armando Mondlane <armandofm@MBP-de-Armando.workgroup>
2017-03-17 06:30:02 +0200

    CRUD Completo de Ponte + Delete via AJAX

--- public/js/custom.js
+++ public/js/custom.js
@@ -77,9 +77,10 @@ $(document).ready(function() {
         var linha = $(this).closest('tr');
         swal({
             title: 'Tem a certeza?',
-            text: 'Não será possivel reverter esta accão, Coloque a sua senha',
             type: 'warning',
             html: '<form class="form-horizontal">' +
+                '<h6>Não será possivel reverter esta accão</h6>' +
+                '<h6 class="red-text">Coloque a sua senha</h6>' +
                 '<div>' +
                 '<div class="input-field col s12">' +
                 '<input id="password" type="password" class="form-control" name="password" required>' +
