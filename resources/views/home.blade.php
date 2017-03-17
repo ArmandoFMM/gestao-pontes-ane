f68c9ee17bfac49ec7fd8c1a183a330fea7eb61e
Armando Mondlane <armandofm@MBP-de-Armando.workgroup>
2017-03-17 06:41:19 +0200

    CRUD Completo de Ponte + Delete via AJAX

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,7 +51,7 @@ Dashboard
                 <ul class="collection">
                     @foreach($pontes_estado_bom as $ponte)
                         <li class="collection-item avatar">
-                            <img src="{{asset('storage/'.$ponte->imagem)}}" alt="" class="circle">
+                            <img src="http://res.cloudinary.com/armandofm/image/upload/pontes-img/{{$ponte->imagem)}}" alt="" class="circle">
                             <span class="title">{{$ponte->nome_ponte}}</span>
                             <p>{{$ponte->estrada->nome_estrada}}<br> {{$ponte->distrito->nome_distrito}}
                             </p>
@@ -85,7 +85,7 @@ Dashboard
                         <ul class="collection">
                         @foreach($pontes_estado_degradada as $ponte)
                             <li class="collection-item avatar">
-                                <img src="{{asset('storage/'.$ponte->imagem)}}" alt="" class="circle">
+                                <img src="http://res.cloudinary.com/armandofm/image/upload/pontes-img/{{$ponte->imagem)}}" alt="" class="circle">
                                 <span class="title">{{$ponte->nome_ponte}}</span>
                                 <p>{{$ponte->estrada->nome_estrada}}<br> {{$ponte->distrito->nome_distrito}}
                                 </p>
@@ -119,7 +119,7 @@ Dashboard
       <i class="large material-icons">add</i>
     </a>
     <ul>
-      <li><a class="btn-floating teal"><i class="material-icons"></i>Ponte</a></li>
+      <li><a href="{{route('pontes.create')}}" class="btn-floating teal"><i class="material-icons">pin_drop</i></a></li>
       <li><a class="btn-floating yellow darken-1"><i class="material-icons">account_circle</i></a></li>
     </ul>
   </div>
